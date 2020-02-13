f3ad655f6d1f11c3e12ad16ce0b9c98a6b3322e6
Duane Edwards <mail@duaneedwards.net>
2020-02-14 03:49:28 +1000

    Empty repository for new solution commit

--- SwallowExceptions/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("SwallowExceptions")]
-[assembly: AssemblyProduct("SwallowExceptions")]
-[assembly: AssemblyVersion("0.0.5")]
-[assembly: AssemblyFileVersion("0.0.5")]
